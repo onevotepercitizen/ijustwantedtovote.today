468587a9b8c9d8a6036bf77346eda1508d7ab87d
Colin Dean <git@cad.cx>
2016-11-08 09:44:30 -0500

    Adds automatic legislator lookup
    Fixes #1

--- js/main.js
+++ js/main.js
@@ -58,3 +58,64 @@ if (window.addEventListener) { // Mozilla, Netscape, Firefox
 else if (window.attachEvent) { // IE
   window.attachEvent('onload', initializeSurvey);
 }
+
+function lookupLegislatorAddressFor(lat, long){
+  var osUrl = "http://openstates.org/api/v1/legislators/geo/?lat="+ lat +"&long="+long+"&apikey=74a824e4095840e183af1767e3db3b1a"
+  httpGetAsync(osUrl, function(text) {
+    var legislatorArray = JSON.parse(text);
+    var official_names = [];
+    var official_addresses = [];
+    for(index in legislatorArray){
+      var legislator = legislatorArray[index];
+      var last_name = legislator.last_name;
+      var title = getTitleForChamber(legislator.chamber);
+      var name = title + ' ' + last_name;
+      official_names.push(name);
+
+      var address = legislator.offices[0].address;
+      official_addresses.push(name + "\n" + address);
+    }
+    document.getElementById('official-name').innerText = official_names.join(' & ')
+
+    if(official_addresses.length > 0){
+      var to = document.getElementById('to-address');
+      to.className = '';
+      var addrs = official_addresses.join("\n\n");
+      var replaced = addrs.replace(new RegExp("\n", 'g'), "<br/>");
+      to.innerHTML = replaced;
+    }
+  });
+}
+
+function getTitleForChamber(chamber) {
+  if(chamber == 'upper') return 'Senator';
+  if(chamber == 'lower') return 'Representative';
+}
+
+// http://stackoverflow.com/a/4033310/204052
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+function geocodingAvailable(){
+  window.geocoder = new google.maps.Geocoder();
+  document.getElementById('survey').elements.address.addEventListener('change', function(event) {
+    window.geocoder.geocode( {'address': event.target.value }, function(results, status){
+      if( status == 'OK') {
+        var lat = results[0].geometry.location.lat();
+        var long = results[0].geometry.location.lng();
+
+        lookupLegislatorAddressFor(lat, long);
+      } else {
+        console.log('Geocode failed: ' + status);
+      }
+    });
+  });
+}
